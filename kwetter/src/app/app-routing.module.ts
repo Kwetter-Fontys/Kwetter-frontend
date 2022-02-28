ab7949054256a893036874f0718d8e892cf9869e
Bakker,Sebas S <sebasbakker8@hotmail.com>
2022-02-28 17:33:44 +0100

    Added basic styling

--- kwetter/src/app/app-routing.module.ts
+++ kwetter/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+  { path: 'profile', component: ProfilePageComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- kwetter/src/app/app.module.ts
+++ kwetter/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ModerationComponent } from './moderation/moderation.component';
+import { AdministrationComponent } from './administration/administration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    ProfilePageComponent,
+    RegistrationComponent,
+    ModerationComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
