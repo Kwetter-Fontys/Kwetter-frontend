7d79fc9ab94281674e0a38748482e5b681811597
Bakker,Sebas S <sebasbakker8@hotmail.com>
2022-03-29 22:45:52 +0200

    chagned user import again

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs";
-import { User } from '.././interfaces/user';
+import { User } from 'src/app/interfaces/user';
 
 @Injectable({
   providedIn: 'root'
