78c95125f761e597c2307a92c3d3dff88da14e5e
Bakker,Sebas S <sebasbakker8@hotmail.com>
2022-04-09 14:49:27 +0200

    Fixed imports so it can be pushed

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/interfaces/User';
-import { TweetService } from '../services/tweet.service';
-import { Tweet } from '../interfaces/tweet';
+import { TweetService } from 'src/app/services/tweet.service';
+import { Tweet } from 'src/app/interfaces/tweet';
 
 @Component({
   selector: 'app-profile-page',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://user-service.sebananasprod.nl/api/",
+  userApiUrl: "https://user-service.sebananasprod.nl/api/",
   tweetApiUrl: "https://tweet-service.sebananasprod.nl/api/"
 };
